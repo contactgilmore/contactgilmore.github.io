6d81058dc6c4af0c002ba99a49a92468f3bb689e
islestoaugusta <contactgilmore@gmail.com>
2025-06-17 15:42:16 -0600

    Added blog section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('my-blog-link')) {
+  document.getElementById('my-blog-link').addEventListener('click', () => {
+    document.getElementById('my-blog-section').scrollIntoView({behavior: "smooth"})
+  })
+}
