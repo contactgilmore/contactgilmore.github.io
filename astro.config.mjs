f2b463fd3ec3b4880157a9dfd6b73750a598870b
contactgilmore <contactgilmore@gmail.com>
2026-08-08 04:10:19 -0600

    Generate sitemap for Portfolio 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://contactgilmore.github.io',
   output: 'static',
   trailingSlash: 'always',
+  integrations: [
+    sitemap({
+      namespaces: {
+        news: false,
+        xhtml: false,
+        video: false,
+      },
+    }),
+  ],
 });
